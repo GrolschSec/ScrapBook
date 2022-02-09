240f99f24ffbcb559fca3d71d87602aa62b2827f
Romain Louvrier <romain-louvrier@outlook.fr>
2022-02-09 19:02:42 +0100

    Modificated write_csv function and get_book_info to add a dictionnary and write it into a CSV

--- utils.py
+++ utils.py
@@ -4,28 +4,26 @@ import urllib.request
 import csv
 import os
 
-url = "https://books.toscrape.com/"
 
-
-def make_dir():
-    os.system("mkdir Images")
-    os.system("mkdir CSV_Files")
+def make_dir(dir_name):
+    os.system("mkdir " + dir_name)
+    return None
 
 
 def url_to_jpg(image_url, file_path):
     filename = image_url.split("/")[8]
     full_path = "{}{}".format(file_path, filename)
     urllib.request.urlretrieve(image_url, full_path)
-    # print('{} saved'.format(filename))
     return None
 
 
-def get_book_info(book_url):
+def get_book_info(base_url, book_url):
     """
     This function is used to get all info about a book from his url.
     This function only work with book.toscrape.com.
 
     Args:
+        base_url: Main url of the website.
         book_url: The url of the book.
 
     Return:
@@ -40,15 +38,6 @@ def get_book_info(book_url):
         image_url: The url of the image to descript the book.
     """
     page = requests.get(book_url)
-    upc = ""
-    title = ""
-    pit = ""
-    pet = ""
-    na = ""
-    pd = ""
-    category = ""
-    rr = ""
-    image_url = ""
     if page.ok:
         soup = BeautifulSoup(page.content, "html.parser")
         book_swap = soup.find_all("td", class_=False)
@@ -60,21 +49,33 @@ def get_book_info(book_url):
         pd = soup.find_all("p")[3].text
         category = soup.find_all("a")[3].text
         rr = soup.find("p", class_="star-rating").attrs["class"][1]
-        image_url = url + soup.find("img").attrs["src"].replace("../../", "/")
+        image_url = base_url + soup.find("img").attrs["src"].replace("../../", "/")
+        dico = {
+            "product_url": book_url,
+            "universal_product_code": upc,
+            "title": title,
+            "price_including_tax": pit,
+            "price_excluding_tax": pet,
+            "number_available": na,
+            "product_description": pd,
+            "category": category,
+            "review_rating": rr,
+            "image_url": image_url,
+        }
+        return dico, image_url
     else:
         print("Status Code: " + str(page.status_code))
-    return book_url, upc, title, pit, pet, na, pd, category, rr, image_url
 
 
-def get_category(category_url):
+def get_category(base_url, category_url):
     page = requests.get(category_url)
     soup = BeautifulSoup(page.content, "html.parser")
     links = []
-    category_name = soup.findAll("div", class_="page-header")
+    category_name = soup.find("div", class_="page-header").find("h1").text
     h3s = soup.findAll("h3")
     for h3 in h3s:
         links.append(
-            url + h3.find("a").attrs["href"].replace("../../../", "catalogue/")
+            base_url + h3.find("a").attrs["href"].replace("../../../", "catalogue/")
         )
     while soup.find(class_="next"):
         new_link = category_url.split("/")
@@ -88,79 +89,28 @@ def get_category(category_url):
         h3s = soup.findAll("h3")
         for h3 in h3s:
             links.append(
-                url + h3.find("a").attrs["href"].replace("../../../", "catalogue/")
+                base_url + h3.find("a").attrs["href"].replace("../../../", "catalogue/")
             )
     return category_name, links
 
 
-def write_csv(cat_name, cat_data):
+def write_csv(csv_path, cat_name, dico):
     header = [
         "Product URL",
         "Universal Product Code",
         "Title",
-        "Price Including Tax",
-        "Price Excluding Tax",
-        "Number Available",
-        "Product Description",
+        "Price including tax",
+        "Price excluding tax",
+        "Number available",
+        "Product description",
         "Category",
-        "Review Rating",
-        "Image  URL",
+        "Review rating",
+        "Image URL",
     ]
-    csvfile = open("CSV_Files/" + cat_name, "w", newline="", encoding="utf8")
+    csvfile = open(csv_path + cat_name + ".csv", "w", newline="", encoding="utf8")
     c = csv.writer(csvfile)
     c.writerow(header)
-    for i in range(len(cat_data)):
-        c.writerow(cat_data[i])
-        i += 1
+    for data in dico:
+        c.writerow(data.values())
     csvfile.close()
     return None
-
-
-"""
-def get_all():
-    a = []
-    cat = []
-    header = [
-        "Product URL",
-        "Universal Product Code",
-        "Title",
-        "Price Including Tax",
-        "Price Excluding Tax",
-        "Number Available",
-        "Product Description",
-        "Category",
-        "Review Rating",
-        "Image  URL",
-    ]
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    links = soup.find(class_="nav-list").find("ul").findAll("a")
-    categories = soup.find(class_="nav-list").find("ul").findAll("a")
-    for category in categories:
-        cat.append(category.text.strip().replace(" ", "-") + ".csv")
-    for link in links:
-        a.append(url + link.attrs["href"])
-    i = 0
-    while i != len(a):
-        csvfile = open("CSV_Files/" + cat[i], "w", newline="", encoding="utf8")
-        c = csv.writer(csvfile)
-        c.writerow(header)
-        print("Getting category " + cat[i].replace(".csv", "") + " data !")
-        get_category(a[i])
-        for ii in range(len(book_urls)):
-            row = [
-                book_urls[ii],
-                upcs[ii],
-                titles[ii],
-                pit[ii],
-                pet[ii],
-                na[ii],
-                pd[ii],
-                cate[ii],
-                rr[ii],
-                image_urls[ii],
-            ]
-            c.writerow(row)
-        csvfile.close()
-        i += 1
-"""
