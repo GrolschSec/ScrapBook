01fe3cbdd8cea2e5490519c632a33d2a5465e5d6
Romain Louvrier <romain-louvrier@outlook.fr>
2022-02-09 19:15:37 +0100

    Modifying main.py: ready to get one all category.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from functions import *
+from utils import *
 
-print("ScrapBook: A Scrapper for 'books.toscrape.com'")
-user_input = input("Do you want to scrape the website ? (Y/N) ")
+url = "https://books.toscrape.com/"
+make_dir('CSV_Files')
+make_dir('Images')
 
-if user_input == "Y" or user_input == "y":
-    print("Scraping the whole website...")
-    make_dir()
-    get_all()
-elif user_input == "N" or user_input == "n":
-    print("Come back later")
-else:
-    print("Unknown option !")
+category = get_category(url, 'http://books.toscrape.local/catalogue/category/books/fiction_10/index.html')
+category_name = category[0]
+links = category[1]
+data = []
+for link in links:
+    x = get_book_info(url, link)
+    data.append(x[0])
+    url_to_jpg(x[1], 'Images/')
+write_csv('CSV_Files/', category_name, data)
